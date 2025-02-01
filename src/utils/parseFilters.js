92952d95c83e6088d02870ad95a333e2cd517c63
Olena Oleksandrova <ha240886kev@gmail.com>
2025-02-01 11:36:23 +0200

    parseFilters

--- src/utils/parseFilters.js
+++ src/utils/parseFilters.js
@@ -1,15 +1,28 @@
 import { parseNumber } from './parseNumber.js';
 
-const parseBoolean = (string) => {
-  if (['true', 'false'].includes(string)) return JSON.parse(string);
-  // if (string === 'true') return true;
-  // if (string === 'false') return false;
-  // return undefined;
+const parseContactType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+  const isContactType = (contactType) =>
+    ['work', 'home', 'personal'].includes(contactType);
+  if (isContactType(contactType)) return contactType;
+};
+
+const parseBoolean = (value) => {
+  const isString = typeof value === 'string';
+  if (!isString) return;
+
+  const trimmedValue = value.trim().toLowerCase();
+  if (trimmedValue === 'true') {
+    return true;
+  } else if (trimmedValue === 'false') {
+    return false;
+  }
 };
 
 export const parseFilters = (filter) => {
   return {
-    type: parseBoolean(filter.type),
+    type: parseContactType(filter.type),
     isFavourite: parseBoolean(filter.isFavourite),
   };
 };
