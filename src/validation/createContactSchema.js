a6d05e0352d7b8dbf253e906d60ff8c7898dc7a1
Olena Oleksandrova <ha240886kev@gmail.com>
2025-02-10 00:04:06 +0200

    rework 3 Schema

--- src/validation/createContactSchema.js
+++ src/validation/createContactSchema.js
@@ -1,13 +1,12 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'Username should be a string',
-    'string.min': 'Username should have at least 3 characters',
-    'string.max': 'Username should have at most 20 characters',
-    'any.required': 'Username is required',
-  }),
-  phoneNumber: Joi.number().required(),
-  email: Joi.string().required(),
-  contactType: Joi.string(),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean().default(false),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .default('personal')
+    .required(),
 });
--- src/validation/updateContactSchema.js
+++ src/validation/updateContactSchema.js
@@ -2,7 +2,10 @@ import Joi from 'joi';
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.number(),
-  email: Joi.string(),
-  contactType: Joi.string(),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean().default(false),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .default('personal'),
 });
